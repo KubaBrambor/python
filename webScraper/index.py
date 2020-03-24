be419666dfea0d9e6dee2d3267f56774c47becb4
Jakub Brambor <jbrambor@google.com>
2020-03-24 18:18:24 +0100

    lessons with Beautiful Soup

--- webScraper/index.py
+++ webScraper/index.py
@@ -1,22 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-result = requests.get("https://www.google.com")
+# result = requests.get("https://www.google.com")
 
-print(result.status_code)
-# print(result.headers)
-print("\n")
+# print(result.status_code)
+# # print(result.headers)
+# print("\n")
+
+# src = result.content
+
+# soup = BeautifulSoup(src, 'html.parser')
+# links = soup.find_all("a", class_="gb1")
+
+# # print(links)
+
+# for link in links:
+#     if "Grafika" in link.text:
+#         print(link)
+#         print("\n")
+#         print(link.attrs["href"])
+#         print(link.attrs["class"])
+
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
 
 src = result.content
-
 soup = BeautifulSoup(src, 'html.parser')
-links = soup.find_all("a", class_="gb1")
+print(soup.prettify())
 
-# print(links)
+# urls = []
 
-for link in links:
-    if "Grafika" in link.text:
-        print(link)
-        print("\n")
-        print(link.attrs["href"])
-        print(link.attrs["class"])
+# for h2_tag in soup.find_all("h2"):
+#     a_tag = h2_tag.find("a")
+#     urls.append(a_tag.attrs["href"])
+
+# for url in urls:
+#     print(url)
