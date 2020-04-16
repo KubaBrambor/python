b117d2fd168006bbf37fd05a582981acd892a860
Jakub <kuba.brambor@gmail.com>
2020-04-16 11:12:53 +0200

    finising task manager

--- flask/freeCodeCamp/app.py
+++ flask/freeCodeCamp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,9 +15,48 @@ class List(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = List(content = task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem'
+
+    else:
+        tasks = List.query.order_by(List.date_created).all()
+        return render_template('index.html', tasks = tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = List.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    task = List.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem with updating your task'
+            
+    else:
+        return render_template('update.html', task = task)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
