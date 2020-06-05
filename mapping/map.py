367fe2afde686c8c90d6e8c164a34119fe6d32c7
Jakub Brambor <kuba.brambor@gmail.com>
2020-06-05 23:21:04 +0200

    add circle markers to vulcanos

--- mapping/map.py
+++ mapping/map.py
@@ -8,11 +8,16 @@ name = list(data["NAME"])
 status = list(data["STATUS"])
 elev = list(data["ELEV"])
 
-html_popup = """<h4 style="text-align:center">Volcano information:</h4>
+html_popup = """
+<h4 style="text-align:center">Volcano information:</h4>
 <h3 style="text-align:center"> %s </h3>
 Height: %s 
 </br>
 Status: %s 
+"""
+
+homes_popup = """
+<h4 style="text-align:center">%s</h4> 
 """
 
 multiCoordinates = [[51.130197, 16.961561], [51.072835, 16.989717], [51.061847, 16.978033], [51.098237, 17.668597], [51.043037, 16.198196]]
@@ -33,12 +38,13 @@ fg = folium.FeatureGroup(name="My Map")
 
 
 for coordinates in multiCoordinates:
-    fg.add_child(folium.Marker(location=coordinates, popup=popups[i], icon=folium.Icon(color='green', icon='glyphicon-home', prefix='glyphicon')))
+    iframe = folium.IFrame(html=homes_popup % (popups[i]), width=150, height=50)
+    fg.add_child(folium.Marker(location=coordinates, popup=folium.Popup(iframe), icon=folium.Icon(color='green', icon='glyphicon-home', prefix='glyphicon')))
     i += 1
 
 for lt, ln, nm, st, ev in zip(lat, lon, name, status, elev):
     iframe = folium.IFrame(html=html_popup % (nm, str(ev), st), width=150, height=200)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_elevation(ev), icon='glyphicon-picture', prefix='glyphicon')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), color=color_elevation(ev), fill=True, fill_color=color_elevation(ev), fill_opacity=0.8))
 
 map.add_child(fg)
 
