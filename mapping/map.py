8c723004173c2fb9d5d5def3eab3662c2b4a030e
Jakub Brambor <kuba.brambor@gmail.com>
2020-06-06 00:28:16 +0200

    adding geoJSON

--- mapping/map.py
+++ mapping/map.py
@@ -20,6 +20,19 @@ homes_popup = """
 <h4 style="text-align:center">%s</h4> 
 """
 
+feature = {
+    "color": "red",
+    "weight": 5,
+    "opacity": 0.9
+}
+
+def geoStyle(feature):
+    return {
+    "color": "blue",
+    "weight": 1,
+    "opacity": 0.5
+}
+
 multiCoordinates = [[51.130197, 16.961561], [51.072835, 16.989717], [51.061847, 16.978033], [51.098237, 17.668597], [51.043037, 16.198196]]
 popups = ['Nasz dom :-)', 'Dom Mamy Kuby', 'Dom Taty Kuby', 'Dom Rodziców Izy', 'Dom Dziadków Kuby']
 
@@ -45,6 +58,8 @@ for coordinates in multiCoordinates:
 for lt, ln, nm, st, ev in zip(lat, lon, name, status, elev):
     iframe = folium.IFrame(html=html_popup % (nm, str(ev), st), width=150, height=200)
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), color=color_elevation(ev), fill=True, fill_color=color_elevation(ev), fill_opacity=0.8))
+
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()), style_function=geoStyle))
 
 map.add_child(fg)
 
