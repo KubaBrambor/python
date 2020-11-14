9878d7cb79993adf6c53086759eb9d4c1490418f
Jakub Brambor <kuba.brambor@gmail.com>
2020-11-14 19:09:40 +0100

    adding sign up screen

--- oopCourse/kivyApp/main.py
+++ oopCourse/kivyApp/main.py
@@ -1,19 +1,30 @@
 import kivy 
 from kivy.app import App 
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, ShaderTransition, SlideTransition
 
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
+    def sign_up(self):
+        print("Sign up button clicked.")
+        self.manager.current = "sign_up_screen"
+
+class SignUpScreen(Screen):
     pass
 
-class RootWidget(ScreenManager):
-    pass
+# one way to add widgets
+# class RootWidget(ScreenManager):
+#     def __init__(self):
+#         self.transition = SlideTransition()
+
+sm = ScreenManager()
+sm.add_widget(LoginScreen(name="login_screen"))
+sm.add_widget(SignUpScreen(name="sign_up_screen")) 
 
 class MainApp(App):
     def build(self):
-        return RootWidget()
+        return sm
 
 if __name__ == "__main__":
     MainApp().run()
