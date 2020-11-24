ecb61c20403e554a9e2510e2fbebfa10700a3dce
Jakub Brambor <kuba.brambor@gmail.com>
2020-11-24 20:02:32 +0100

    adding login backend logic

--- oopCourse/kivyApp/main.py
+++ oopCourse/kivyApp/main.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, ShaderTransition, SlideTransition, SwapTransition
+from kivy.properties import StringProperty
 import json
 from datetime import datetime
 
@@ -12,6 +13,17 @@ class LoginScreen(Screen):
         print("Sign up button clicked.")
         self.manager.transition = SwapTransition()
         self.manager.current = "sign_up_screen"
+    
+    def login(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+        if username in users.keys():
+            if users[username]["password"] == password:
+                self.manager.current = "sign_up_screen_success"
+            else:
+                self.ids.title.text = "Password is incorrect!"
+        else:
+            self.ids.title.text = f"There is no user {username}"
 
 class SignUpScreen(Screen):
     def add_user(self, username, password):
@@ -36,6 +48,11 @@ class SignUpScreenSuccess(Screen):
         # self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
 
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = "login_screen"
+
 # one way to add widgets
 # class RootWidget(ScreenManager):
 #     def __init__(self):
@@ -45,7 +62,7 @@ sm = ScreenManager()
 sm.add_widget(LoginScreen(name="login_screen"))
 sm.add_widget(SignUpScreen(name="sign_up_screen")) 
 sm.add_widget(SignUpScreenSuccess(name="sign_up_screen_success"))
-
+sm.add_widget(LoginScreenSuccess(name="login_screen_success"))
 class MainApp(App):
     def build(self):
         return sm
