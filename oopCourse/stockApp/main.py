4e429eb2b92a5bcc86351c5bda8116ea84b2e155
Jakub Brambor <kuba.brambor@gmail.com>
2020-12-03 19:39:13 +0100

    styling news page

--- oopCourse/stockApp/main.py
+++ oopCourse/stockApp/main.py
@@ -13,6 +13,9 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.widget import Widget
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle, Line
 from kivy.core.window import Window
 from datetime import datetime
 
@@ -43,16 +46,23 @@ class MainScreen(Screen):
         gridNavi.add_widget(Button(text="Find stock"))
         gridNavi.add_widget(Button(text="Wallet"))
         
+        def line(self):
+            with self.canvas:
+                Line(points=(0, 1, 2, 3, 4, 5))
+                Color(1, 0, 0, .5, mode='rgba')
+                Rectangle(pos=self.pos, size=self.size)
+
         #Layout two with stock 
-        gridNews = GridLayout(cols=1, size_hint_y=None)
+        gridNews = GridLayout(cols=1, size_hint_y=None, padding=(50,50))
         gridNews.bind(minimum_height=gridNews.setter('height'))
         for i in range(len(self.data['articles'])):
-            gridNews.add_widget(Image(source=self.data['articles'][i]['urlToImage']))
-            gridNews.add_widget(Label(text=self.data['articles'][i]['title'], size_hint_y=None))
-            gridNews.add_widget(Label(text=self.data['articles'][i]['description'], size_hint_y=None))
-            gridNews.add_widget(Button(text="Go to", on_press=lambda x: print(self.data['articles'][i]['url'])))
-            # self.build_label(self.data['articles'][i]['title'])
-            # self.build_label(self.data['articles'][i]['description'])
+            gridNews.add_widget(Label(text=self.data['articles'][i]['title'], size_hint_y=None, size=(900,80), text_size=(200, None), font_size="20sp"))
+            gridNews.add_widget(Label(text=self.data['articles'][i]['source']['name'], size_hint_y=None, size=(900,30), text_size=(200, None), font_size="10sp"))
+            gridNews.add_widget(Label(padding=(5,50),text=self.data['articles'][i]['description'], size_hint_y=None, size=(900,200), text_size=(200, None), font_size="15sp"))
+            
+            # gridNews.add_widget(Widget(canvas=(Line(points=(0, 1, 2, 3, 4, 5)),Color(1, 0, 0, .5, mode='rgba'))))
+            # gridNews.add_widget(Button(text="Go to", on_press=lambda x: print(self.data['articles'][i])))
+            
         scrollviewNews = ScrollView(size_hint=(1, None),size=(Window.width, Window.height))
         scrollviewNews.add_widget(gridNews)
         gridNavi.add_widget(scrollviewNews)
